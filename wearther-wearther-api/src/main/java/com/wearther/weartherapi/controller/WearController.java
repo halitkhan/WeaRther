b13016e5260dcdfcfbbd2b92c80fbb75e08a92a6
Halit Kaan KAÇAR <halitkaan.kacar@std.yeditepe.edu.tr>
2023-01-02 18:31:39 +0300

    Updated JavaScript fetching data and connected some html tags with js Added some HTML attributes Added CrossOrigin annotation to wear-api

--- wearther-wearther-api/src/main/java/com/wearther/weartherapi/controller/WearController.java
+++ wearther-wearther-api/src/main/java/com/wearther/weartherapi/controller/WearController.java
@@ -4,13 +4,11 @@ import com.wearther.weartherapi.dto.model.Clothes;
 import com.wearther.weartherapi.service.WearService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/clothes")
 public class WearController {
